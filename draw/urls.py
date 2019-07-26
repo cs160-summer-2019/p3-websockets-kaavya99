b27eb5555fb72f902864c4664d23d8e69e0584bf
kaavyasinghal <kaavyasinghal@berkeley.edu>
2019-07-25 22:00:44 -0400

    project

--- draw/urls.py
+++ draw/urls.py
@@ -1,10 +1,16 @@
 # chat/urls.py
 from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+#    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url('tasks/', views.tasks, name='tasks'),
+    url('largeboard/', views.largeboard, name='largeboard'),
+    url('largeboarddragon/', views.largeboarddragon, name='largeboarddragon'),
+    url('tasksdragon/' , views.tasksdragon, name='tasksdragon'),
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -9,3 +9,15 @@ def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+  
+def tasks(request):
+  return render(request, 'draw/tasks.html', {})
+
+def largeboard(request):
+  return render(request, 'draw/largeboard.html', {})
+
+def largeboarddragon(request):
+  return render(request, 'draw/largeboarddragon.html', {})
+
+def tasksdragon(request):
+  return render(request, 'draw/tasksdragon.html', {})
